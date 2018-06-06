65406c5cb6e02160e74687cdd168ede274685878
Dasha <etterwin.yll@gmail.com>
2018-06-06 14:26:44 +0300

    update

--- js/main.js
+++ js/main.js
@@ -122,9 +122,8 @@ $(document).ready(function () {
         }
     });
 
-
+    // Открыть список
     $(document).on('click', '.open-list', function () {
-
         if ($(this).hasClass('cart-select-wrapper')) {
             $(this).addClass('active');
             $(this).siblings('.choose-list').addClass('active');
@@ -133,9 +132,34 @@ $(document).ready(function () {
             $(this).closest('.cart-select-wrapper').addClass('active');
             $(this).parent('.cart-select-wrapper').siblings('.choose-list').addClass('active');
         }
+    });
 
+    // Анимация лейбла на странице регистрации
+    let show = 'show';
+    $('input').on('checkval', function () {
+        let label = $(this).next('label');
+        if(this.value !== '') {
+            label.addClass(show);
+        } else {
+            label.removeClass(show);
+        }
+    }).on('keyup', function () {
+        $(this).trigger('checkval');
+    });
 
+    // Маска для телефона
+    $(document).on('input #phone_num', function () {
+        $("#phone_num").mask("+7(999) 999-99-99");
+    });
+
+    // если галочка проставлена, то кнопка становится кликабельной
+    $(document).on('click', '.check', function () {
+        if ($(this).find('input[type = checkbox]').prop('checked') === true) {
+            $('.auth-btn-wrapper.sign-in').addClass('active-btn');
+        }
+        else  {
+            $('.auth-btn-wrapper.sign-in').removeClass('active-btn');
+        }
     });
 
 });
-
