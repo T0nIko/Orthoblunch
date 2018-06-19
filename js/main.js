fe1a7ce7004d8808d58679b57b929123b8bca496
Dasha <etterwin.yll@gmail.com>
2018-06-19 15:05:10 +0300

    update

--- js/main.js
+++ js/main.js
@@ -65,6 +65,7 @@ $(document).ready(function () {
         if (!popup.is(e.target) && popup.has(e.target).length === 0) {
             $(popup).removeClass('active');
             $('.navigation-item').removeClass('next');
+            enableScroll();
         }
     });
 
@@ -208,18 +209,26 @@ $(document).ready(function () {
         },
     });
 
-    if (window.screen.width < 768) {
-        // Слайдер промсмотренных
-        let mobil_slider = new Swiper('.mobil-slider.swiper-container', {
-            grabCursor: true,
-            slidesPerView: 'auto',
-            loop: true,
-            navigation: {
-                nextEl: '.catalog-special.next',
-                prevEl: '.catalog-special.prev',
-            },
-        });
+    function mobSlider() {
+        if (window.screen.width < 768) {
+            // Слайдер промсмотренных
+            let mobil_slider = new Swiper('.mobil-slider.swiper-container', {
+                grabCursor: true,
+                slidesPerView: 'auto',
+                loop: true,
+                navigation: {
+                    nextEl: '.catalog-special.next',
+                    prevEl: '.catalog-special.prev',
+                },
+            });
+        }
     }
+
+    mobSlider();
+
+    $(window).resize(function() {
+        mobSlider();
+    });
 
     $(document).on('click', '.product-item-img', function () {
         let img_addess = $(this).attr('src');
@@ -302,15 +311,53 @@ $(document).ready(function () {
     // Открыть попап
     $(document).on('click', '.open-container', function () {
         let container = $(this).data('containerid');
-        console.log(container);
         $('[data-container = ' + container + ']').addClass('active');
+
+        if ( container === 'add_item') {
+            disableScroll();
+        }
     });
 
     // Закрыть попап
     $(document).on('click', '.close-container', function () {
         let container = $(this).data('containerid');
-        console.log(container);
         $('[data-container = ' + container + ']').removeClass('active');
+        enableScroll();
     });
+
+    // Пока поп-ап активен, запретить скролл
+    let keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+    function preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault)
+            e.preventDefault();
+        e.returnValue = false;
+    }
+
+    function preventDefaultForScrollKeys(e) {
+        if (keys[e.keyCode]) {
+            preventDefault(e);
+            return false;
+        }
+    }
+
+    function disableScroll() {
+        if (window.addEventListener) // older FF
+            window.addEventListener('DOMMouseScroll', preventDefault, false);
+        window.onwheel = preventDefault; // modern standard
+        window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+        window.ontouchmove  = preventDefault; // mobile
+        document.onkeydown  = preventDefaultForScrollKeys;
+    }
+
+    function enableScroll() {
+        if (window.removeEventListener)
+            window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        window.onmousewheel = document.onmousewheel = null;
+        window.onwheel = null;
+        window.ontouchmove = null;
+        document.onkeydown = null;
+    }
 
 });
